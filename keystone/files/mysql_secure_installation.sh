7cdd018c54c280a74e46cdcb307f721792b50124
Christian Berendt <berendt@b1-systems.de>
2014-10-23 16:08:01 +0200

    Vagrantbox with a basic OpenStack Identity Service (keystone)

---
+++ keystone/files/mysql_secure_installation.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+expect -c "
+ 
+set timeout 3
+spawn mysql_secure_installation
+ 
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+ 
+expect \"root password?\"
+send \"y\r\"
+ 
+expect \"New password:\"
+send \"password\r\"
+ 
+expect \"Re-enter new password:\"
+send \"password\r\"
+ 
+expect \"Remove anonymous users?\"
+send \"y\r\"
+ 
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+ 
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+ 
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+ 
+expect eof
+"
+
+touch /tmp/mysql_secure_installation.done
