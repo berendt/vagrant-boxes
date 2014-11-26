a8e1a2e002360a66972363eaf7d67dd7637cc12b
Christian Berendt <mail@cberendt.net>
2014-11-26 13:38:06 +0100

    [packstack] do not use patches and do not run packstack after provisioning

--- packstack/run.sh
+++ packstack/run.sh
@@ -6,5 +6,3 @@ vagrant up
 vagrant reconfig
 vagrant sandbox on
 vagrant ssh controller -c '/home/vagrant/initialize.sh'
-vagrant ssh controller -c '/home/vagrant/patch.sh'
-vagrant ssh controller -c 'packstack --answer-file /home/vagrant/packstack.answers'
