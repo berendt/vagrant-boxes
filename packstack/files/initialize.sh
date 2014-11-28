3ef2ac2495dc85188a4789cd705dd12cecb2f470
Christian Berendt <mail@cberendt.net>
2014-11-28 17:52:01 +0100

    [packstack] use dedicated systems for storage and shared services

--- packstack/files/initialize.sh
+++ packstack/files/initialize.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-for node in compute001 compute002 network storage controller; do
+for node in compute001 compute002 network storage controller filer shared; do
     ssh-keyscan $node >> /home/vagrant/.ssh/known_hosts
 done
 chown vagrant:vagrant /home/vagrant/.ssh/known_hosts
 
-for node in compute001 compute002 network storage controller; do
+for node in compute001 compute002 network storage controller filer shared; do
     ssh $node "sudo mkdir -p /root/.ssh"
     ssh $node "sudo chmod 700 /root/.ssh"
     ssh $node "sudo cp /home/vagrant/.ssh/* /root/.ssh"
