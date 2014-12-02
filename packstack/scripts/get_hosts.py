3eb69804dc34e7070d80c6eb45ec6003473413d8
Christian Berendt <mail@cberendt.net>
2014-12-02 23:22:02 +0100

    [packstack] node --> compute

--- packstack/scripts/get_hosts.py
+++ packstack/scripts/get_hosts.py
@@ -7,6 +7,6 @@ config = yaml.load(open('config.yaml'))
 for name, value in config['address'].items():
     if name == 'compute':
         for index, _ in enumerate(value):
-            print("node%d" % (index + 1))
+            print("compute%d" % (index + 1))
     else:
         print(name)
