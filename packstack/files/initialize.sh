ca5686d3f8d9e2cea03cc5e7ae7b3f9ab6bb555d
Christian Berendt <mail@cberendt.net>
2014-12-02 20:32:16 +0100

    [packstack] fix initialize.sh script

--- packstack/files/initialize.sh
+++ packstack/files/initialize.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-for node in compute001 compute002 network storage controller filer shared; do
+for node in $(sed -n '/<<< Packstack >>>/{:a;n;/>>> Packstack <<</b;p;ba}' /etc/hosts | awk '{ print $2 }'); do
     ssh-keyscan $node >> /home/vagrant/.ssh/known_hosts
 done
+
 chown vagrant:vagrant /home/vagrant/.ssh/known_hosts
 
-for node in compute001 compute002 network storage controller filer shared; do
+for node in $(sed -n '/<<< Packstack >>>/{:a;n;/>>> Packstack <<</b;p;ba}' /etc/hosts | awk '{ print $2 }'); do
     ssh $node "sudo mkdir -p /root/.ssh"
     ssh $node "sudo chmod 700 /root/.ssh"
     ssh $node "sudo cp /home/vagrant/.ssh/* /root/.ssh"
