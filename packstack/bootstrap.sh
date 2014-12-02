e8beafde72a0089eb8f951817cc99ff61e498216
Christian Berendt <mail@cberendt.net>
2014-12-02 16:29:51 +0100

    [packstack] several improvements
    * use configuration file for Vagrantfile
    * use script to generate a list of all nodes from the configuration file
    * make used resources configurable
    * replace static entries in files/hosts

--- packstack/bootstrap.sh
+++ packstack/bootstrap.sh
@@ -5,7 +5,7 @@ set -x
 JOBS=2
 
 run_parallel() {
-    cat nodes | xargs -n 1 -P $JOBS -I BOX sh -c "vagrant $* BOX 2>&1 >> log/BOX.log"
+    python get_hosts.py | xargs -n 1 -P $JOBS -I BOX sh -c "vagrant $* BOX 2>&1 >> log/BOX.log"
 }
 
 echo "$(date) brining up all VMs"
